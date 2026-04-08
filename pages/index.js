a54d203d9ae6c742f24ec6158a8787389e246b99
Oliveirasdaniel <exterminado513@gmail.com>
2026-04-08 14:56:17 +0000

    estilização e polir o codigo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function home(){
-    return <h1> o Vascão sera campeão da libertadores de 2027!💢 </h1>
+function home() {
+  return <h1> o Vascão sera campeão da libertadores de 2027!💢 </h1>;
 }
 
 export default home;
