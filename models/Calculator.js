7e5fd0fb910f44121b3d7713b9b05179956af764
Oliveirasdaniel <exterminado513@gmail.com>
2026-05-13 16:21:37 +0000

    adiciona o novo

--- models/Calculator.js
+++
@@ -1,9 +0,0 @@
-function somar(number1, number2) {
-  if (typeof numero1 !== "number") {
-    return "error";
-  }
-  return number1 + number2;
-}
-
-exports.somar = somar;
-console.log("ola popsdev!");
